7694e36e81ef6cee4fdda71633b5133d5d54db76
fluxfw <fluxfw@fluxlabs.ch>
2022-04-22 13:09:14 +0200

    build

--- bin/copy-libs-for-development.sh
+++ bin/copy-libs-for-development.sh
@@ -8,9 +8,14 @@ if [ -z `command -v copy-from-docker-image` ]; then
 fi
 
 if [ -n "$CI_REGISTRY" ] && [ -n "$CI_PROJECT_NAMESPACE" ]; then
-    image="$CI_REGISTRY/$CI_PROJECT_NAMESPACE/flux-markdown-to-html-converter-api:latest"
+    image="$CI_REGISTRY/$CI_PROJECT_NAMESPACE/flux-markdown-to-html-converter-api"
 else
-    image="docker-registry.fluxpublisher.ch/flux-markdown-to-html-converter/api:latest"
+    image="docker-registry.fluxpublisher.ch/flux-markdown-to-html-converter/api"
 fi
 
-(cd "`dirname "$0"`/.." && copy-from-docker-image "$image" /flux-markdown-to-html-converter-api/libs libs)
+tag="$1"
+if [ -z "$tag" ]; then
+    tag="latest"
+fi
+
+(cd "`dirname "$0"`/.." && copy-from-docker-image "$image:$tag" /flux-markdown-to-html-converter-api/libs libs)
